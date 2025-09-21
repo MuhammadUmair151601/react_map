cd5a0e352f4652447f8efe758f974d95724e10ad
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-09-21 12:27:01 +0500

    map method and if statement done

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+// import React from 'react'
+
+// const App = () => {
+//   let players = ["afridi", "Fawad", "Usman","shayan"]
+//   return (
+//     <>
+//     <h1>Players in the team:</h1>
+//     {players.map(player=> (
+//       <li key={player}>{player}</li>
+//     ))}
+//     <ul>
+//       <li></li>
+//     </ul>
+//     </>  
+//     )
+// }
+
+// export default App
+
+// now if there is nothing in the list:
+
+// import React from 'react'
+
+// const App = () => {
+//   // let players = []
+//   let players = ["afridi", "Fawad", "Usman","shayan"]
+  
+//   if (players.length=== 0){
+//     return <h1>I am still hungry</h1>
+//   }
+
+
+//   return (
+//     <>
+//     <h1>Players in the team:</h1>
+//     {players.map(player=> (
+//       <li key={player}>{player}</li>
+//     ))}
+//     <ul>
+//       <li></li>
+//     </ul>
+//     </>  
+//     )
+// }
+
+// export default App
+
+// Now if we want to use if statement inside the block:
+
+// export default App
+
+// now if there is nothing in the list:
+
+import React from 'react'
+
+const App = () => {
+  let players = []
+  // let players = ["afridi", "Fawad", "Usman","shayan"]
+  
+//   return (
+//     <>
+//     <h1>Players in the team:</h1>
+//     {players.length===0 ? <h1>I am still hungry</h1>: null}
+//     {players.map(player=> (
+//       <li key={player}>{player}</li>
+//     ))}
+//     <ul>
+//       <li></li>
+//     </ul>
+//     </>  
+//     )
+// }
+
+// export default App
+
+// similarly we can use logical operators like &&
