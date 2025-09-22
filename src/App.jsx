403be3f5839f84045899cf9b9d43add7535fe30c
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-09-22 15:11:01 +0500

    Use state and form handling done

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@
 //     <ul>
 //       <li></li>
 //     </ul>
-//     </>  
+//     </>
 //     )
 // }
 
@@ -24,12 +24,10 @@
 // const App = () => {
 //   // let players = []
 //   let players = ["afridi", "Fawad", "Usman","shayan"]
-  
+
 //   if (players.length=== 0){
 //     return <h1>I am still hungry</h1>
 //   }
-
-
 //   return (
 //     <>
 //     <h1>Players in the team:</h1>
@@ -39,7 +37,7 @@
 //     <ul>
 //       <li></li>
 //     </ul>
-//     </>  
+//     </>
 //     )
 // }
 
@@ -51,12 +49,12 @@
 
 // now if there is nothing in the list:
 
-import React from 'react'
+// import React from 'react'
 
-const App = () => {
-  let players = []
-  // let players = ["afridi", "Fawad", "Usman","shayan"]
-  
+// const App = () => {
+//   let players = []
+// let players = ["afridi", "Fawad", "Usman","shayan"]
+
 //   return (
 //     <>
 //     <h1>Players in the team:</h1>
@@ -67,10 +65,82 @@ const App = () => {
 //     <ul>
 //       <li></li>
 //     </ul>
-//     </>  
+//     </>
 //     )
 // }
 
 // export default App
 
 // similarly we can use logical operators like &&
+// import React from 'react'
+
+// const App = () => {
+//   let players = []
+// let players = ["afridi", "Fawad", "Usman","shayan"]
+
+//   return (
+//     <>
+//     <h1>Players in the team:</h1>
+//     {players.length===0 ? <h1>I am still hungry</h1>: null}
+//     {players.map(player=> (
+//       <li key={player}>{player}</li>
+//     ))}
+//     <ul>
+//       <li></li>
+//     </ul>
+//     </>
+//     )
+// }
+
+// export default App
+
+//******************************************USE STATE HOOKS IN REACT:
+// import React from "react";
+// import React, { useState } from "react";
+
+// const App = () => {
+//   let [count, setCount] = useState(0);
+//   function incrementCount() {
+//     setCount(count => count + 1);
+//     setCount(count => count + 1);
+
+//   }
+//   function decrementCount() {
+//     setCount((count = count - 1));
+//   }
+
+//   return (
+//     <div>
+//       <h1>Counter: {count}</h1>
+//       <button onClick={incrementCount}>Increment</button>
+//       <button onClick={decrementCount}>Decrement</button>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// **************************FORM HANDLING IN REACT:
+
+// import React from 'react'
+
+// const App = () => {
+//   const submithandler  = (e) => {
+//       e.preventDefault();
+//     }
+
+//     return(
+//     <div>
+//       <form onSubmit={(e)=>{
+//         submithandler(e)
+//       }}>
+//         <input className='px-5 py-3 bg-green-500 border rounded text-white font-bold m-5' type="text"            placeholder='Enter your name'/>
+//         <button className='px-5 py-3 bg-blue-500 border rounded text-white font-bold '>Submit</button>
+//       </form>
+//     </div>
+//   )
+// }
+
+// export default App
+
+//**********************************TWO WAY BINDING IN REACT:
